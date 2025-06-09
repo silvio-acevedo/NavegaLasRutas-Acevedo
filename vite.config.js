4d46dca685b2737d5fa73dd070900abde627f6f3
silvio-acevedo <silvio-acevedo@outlook.com>
2025-06-09 19:16:51 -0300

    NavegaLasRutas-Acevedo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/NavegaLasRutas-Acevedo/",
   plugins: [react()],
+  base: "https://silvio-acevedo.github.io/NavegaLasRutas-Acevedo/",
 });
